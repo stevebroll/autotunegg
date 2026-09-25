7e57c02c9f95143d240bfc73b194782bf9e11fef
stevebroll <sbroll@umich.edu>
2026-09-25 12:56:05 -0400

    autotune outer loop for sigma2 estimation

--- src/autotunegg_bcd.cpp
+++ src/autotunegg_bcd.cpp
@@ -77,6 +77,93 @@ List autotune_gg(arma::mat xin,
     y = y - ymean;
   }
 
+// OUTER LOOP ----
+
+  while (error  > tolerance && iteration <= iter_max){
+    lambda_effective = lambda_value * sigma2hat * tau;
+
+    old_support_set = support_set;
+    s = support_set.size();
+    if(s > 0){
+      support_set.erase(support_set.begin(), support_set.end());
+    }
+    old_beta = beta;
+
+    // Group-wise iterative updates to beta and r
+    for(int g = 0; g < gmax; g++){
+      gactive = active_indices[g];
+      idx = arma::find(group == gactive);
+
+      arma::mat xg = x.cols(idx);
+      r = r + (xg * beta(idx));
+      beta_temp(idx) = xg.t() * r;
+      double penscale = sqrt(pg[gactive]) * lambda_effective / (norm(xg * beta_temp(idx),2));
+      beta(idx) = std::max((1 - penscale), 0.0) * beta_temp(idx);
+      r = r - (xg * beta(idx));
+
+    }
+
+    // Partial residual ranking
+      for(int g = 0; g < gmax; g++){
+        idx = arma::find(group == g);
+        arma::vec partial_res = r + (x.cols(idx) * beta(idx));
+        partial_res_l2(g) = norm(partial_res, 2);
+      }
+
+    active_indices = seq_len(gmax) - 1;
+    std::sort(active_indices.begin(), active_indices.end(),
+              [&partial_res_l2](int a, int b) {
+                return partial_res_l2[a] > partial_res_l2[b];
+              });
+
+
+    while((psum + pg[gactive]) < max_no_of_preds){
+      k++;
+      xg = x.cols(idx);
+      arma::mat xtx = xg.t() * xg;
+      betahat = xg.t() * ytemp;
+      yhat = xg * betahat;
+      arma::mat Sigma = arma::mat(pg[gactive], pg[gactive], arma::fill::eye) * sigma2hat;
+      double f_stat = arma::as_scalar(betahat.t() * Sigma.i() * betahat);
+      double cutoff = R::qf(1-alpha, pg[gactive], n - psum - pg[gactive], true, false) * pg[gactive];
+
+      if(f_stat < cutoff){
+        break;
+      } else {
+        ytemp = ytemp - yhat;
+        support_set.push_back(gactive);
+        psum = psum + pg[gactive];
+        sigma2hat = pow(norm(ytemp,2),2)/(n - psum);
+        gactive = active_indices[k];
+        idx = arma::find(group == gactive);
+      }
+      iter++;
+
+    }
+
+    vec_sig_beta_count[iteration - 1] = iter;
+    sigma2_seq[iteration - 1] = sigma2hat;
+
+
+    beta_crit = abs(beta - old_beta)/ (1 + abs(beta));
+    error = beta_crit.max();
+
+    if (trace_it) {Rcout << "\rIteration: " << iteration << std::flush;}
+    iteration++;
+
+
+    if (setdiff(support_set, old_support_set).size() == 0) {
+      flag--;
+    } else {
+      flag = 1;
+    }
+
+    if (flag == 0) {
+      break;
+    }
+
+  }
+  iteration--;
 
 
 
